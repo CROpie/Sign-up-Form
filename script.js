0c06461f2542c2304913ff2774fca7a416a0540f
CROpie <96667010+CROpie@users.noreply.github.com>
2023-03-25 21:36:56 +1100

    completed lhs

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ password2 = document.querySelector("#password2-input");
 
 submit_button.addEventListener("click", checkPassword);
 
+password1.addEventListener("change", checkEmpty);
+password2.addEventListener("change", checkEmpty);
+
 function checkPassword() {
     if (password1.value === password2.value) {
         alert("no problems");
@@ -14,5 +17,14 @@ function checkPassword() {
         password1.style.borderColor = "red";
         password2.style.borderColor = "red";
         return false;
+    }
+}
+
+function checkEmpty() {
+    if (!password1.value) {
+        password1.style.borderColor = "lightgrey";
+    }
+    if (!password2.value) {
+        password2.style.borderColor = "lightgrey";
     }
 }
